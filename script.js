745ac101561b919825e063d6f6e02c2ba07dba98
VITNT <visotttieng@gmail.com>
2026-09-05 22:40:54 +0700

    Add apps feature and auto-focus app on click

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/* Real Time clock */
+// Real Time clock
 function updateTime() {
   var current_time = new Date().toLocaleString();
   var timeElement = document.getElementById("time");
@@ -7,9 +7,7 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-/* Drag window feature */
-dragElement(document.getElementById("window"));
-
+//  Drag window feature
 function dragElement(element) {
   var initialX = 0;
   var initialY = 0;
@@ -18,6 +16,7 @@ function dragElement(element) {
 
   if (document.getElementById(element.id + "-handle")) {
     document.getElementById(element.id + "-handle").onmousedown = startDragging;
+    addWindowTapHandler(element);
   } else {
     element.onmousedown = startDragging;
   }
@@ -46,24 +45,88 @@ function dragElement(element) {
   }
 }
 
+dragElement(document.getElementById("window"));
+dragElement(document.getElementById("poseidon-md-window"));
+
 // Open-Close window
 var welcomeScreen = document.querySelector("#window");
+var poseidonWindow = document.querySelector("#poseidon-md-window");
+var topBar = document.querySelector("#top-bar");
+var maxIndex = 1;
 
-function closeWindow() {
-  welcomeScreen.style.display = "none";
+function closeWindow(element) {
+  element.style.display = "none";
 }
 
-function openWindow() {
-  welcomeScreen.style.display = "flex";
+function openWindow(element) {
+  element.style.display = "flex";
+  maxIndex++;
+  element.style.zIndex = maxIndex;
+  topBar.style.zIndex = maxIndex + 1;
 }
 
-var welcomeScreenClose = document.querySelector("#welcome-close-button");
-var welcomeScreenOpen = document.querySelector("#welcome-open-button");
+function closeWindowOnClick(element, window) {
+  element.addEventListener("click", () => {
+    closeWindow(window);
+  });
+}
 
-welcomeScreenClose.addEventListener("click", function () {
-  closeWindow();
-});
+function openWindowOnClick(element, window) {
+  element.addEventListener("click", () => {
+    clickIcon(element, window);
+  });
+}
 
-welcomeScreenOpen.addEventListener("click", function () {
-  openWindow();
-});
+openWindowOnClick(
+  document.querySelector("#welcome-open-button"),
+  welcomeScreen,
+);
+
+closeWindowOnClick(
+  document.querySelector("#welcome-close-button"),
+  welcomeScreen,
+);
+
+openWindowOnClick(
+  document.querySelector("#poseidon-md-open-button"),
+  poseidonWindow,
+);
+
+closeWindowOnClick(
+  document.querySelector("#poseidon-md-close-button"),
+  poseidonWindow,
+);
+
+function addWindowTapHandler(element) {
+  element.addEventListener("mousedown", () => {
+    handleWindowTap(element);
+  });
+}
+
+function handleWindowTap(element) {
+  maxIndex++;
+  element.style.zIndex = maxIndex;
+  topBar.style.zIndex = maxIndex + 1;
+}
+
+// Open-Close Apps
+var selectedIcon = undefined;
+
+function selectIcon(element) {
+  element.classList.add("selected");
+  selectedIcon = element;
+}
+
+function deselectIcon(element) {
+  element.classList.remove("selected");
+  selectedIcon = undefined;
+}
+
+function clickIcon(element, window) {
+  if (element.classList.contains("selected")) {
+    deselectIcon(element);
+    openWindow(window);
+  } else {
+    selectIcon(element);
+  }
+}
